96c63a9d697442b0a88dc19da312a5552b960eae
David Turner <david@davidturner.name>
2011-12-18 13:57:30 +0000

    Overhaul of code
    I've learnt a lot about jQuery since I last updated this plugin. I've
    updated the code to reflect this, which *should* improve it's
    performance, especially on sites with a lot of images.

--- _js/jquery.imgslide.js
+++ _js/jquery.imgslide.js
@@ -1,65 +1,58 @@
 /*
- * jQuery Image Slider v0.2
+ * jQuery Image Slider v0.4
  *
  * Copyright (c) 2010 David Turner
- * Site: http://david,turner.name/
+ * Site: http://davidturner.name/
  *
  */   
 
 $.fn.imgslide = function(options) {
 
-	var defaults = {
-		height: 220
-	};
-
-	var options = $.extend(defaults, options);
-
-	$(this).each(function(){
-		var imgsrc = $(this).children(":first-child").attr("src");
-		var imgalt = $(this).children(":first-child").attr("alt");
-		var imgheight = $(this).children(":first-child").height();
-		$(this).after('<p style="visibility:hidden;line-height:0;margin:0;">.</p>');
-		if(imgheight > options.height){                           
-			$(this).attr({'data-height' : imgheight});                
-			$(this).children(":first-child").wrap('<p class="wrap"></p>');
-			$(this).children(":first-child").after('<p class="bottom">Click to expand image to full height.</p>');
-			$(this).children(":first-child").before('<span class="caption">'+imgalt+'</span>');
-			$(this).children("p.wrap").addClass("covered");                                
-			//$(this).children("p.wrap").css("height:"+options.height+"px");   
-			$(this).children("p.wrap").animate({ 
-		    height: options.height+'px'
-		  }, 1500 );
-			$(this).addClass("cursor");
-			$(this).addClass("minimal"); 
-		}else{
-			$(this).attr({rel : imgsrc+":"+imgheight+":"+imgalt});
-			$(this).addClass("fullimg");
-			$(this).removeClass("img"); 
-			$(this).children(":first-child").after('<span class="caption">'+imgalt+'</span>');
-		} 
-
-	});   
-	
-	$(this).toggle(function() {                          
-		var height = $(this).attr('data-height');
-		$(this).children("p.wrap").animate({ 
-			height: height + 'px'
-		}, 1500 );
-		$(this).children("p.bottom").html("Click to shrink image to smaller size."); 
-		$(this).children("p.bottom").animate({ 
-			top: height + 'px'
-		}, 1500 , function() {
-		});
-		}, function() {                                  
-			$(this).children("p.wrap").animate({ 
-				height: options.height + 'px' 
-			}, 1500 , function() {
-		});
-
-		$(this).children("p.bottom").html("Click to expand image to full height.");
-		$(this).children("p.bottom").animate({ 
-			top: options.height + 'px'
-		}, 1500 );
-
-	});
-};       
+  var defaults = {
+    height: 220,
+    hideDuration: 0,
+    toggleDuration: 1500
+  };
+  options = $.extend(defaults, options);
+  $(this).each(function(){
+    var img = $(this),
+        imgsrc = img.children(":first-child").attr("src"),
+        imgalt = img.children(":first-child").attr("alt"),
+        imgheight = img.children(":first-child").height();
+    img.after('<p style="visibility:hidden;line-height:0;margin:0;">.</p>');
+    if(imgheight > options.height){
+      img.attr({'data-height' : imgheight});
+      img.children(":first-child")
+        .after('<p class="bottom">Click to expand image to full height.</p>')
+        .before('<span class="caption">'+imgalt+'</span>')
+        .wrap('<p class="wrap"></p>');
+      img.children("p.wrap").addClass("covered").animate({
+        height: options.height+'px'
+      }, options.hideDuration );
+      img.addClass("cursor").addClass("minimal");
+    }else{
+      img.attr({rel : imgsrc+":"+imgheight+":"+imgalt})
+        .addClass("fullimg")
+        .removeClass("img")
+        .children(":first-child").after('<span class="caption">'+imgalt+'</span>');
+    }
+  });
+  $(this).toggle(function() {
+    var img = $(this),
+        height = img.attr('data-height');
+    img.children("p.wrap").animate({
+      height: height + 'px'
+    }, options.toggleDuration );
+    img.children("p.bottom").html("Click to shrink image to smaller size.").animate({
+      top: height + 'px'
+    }, options.toggleDuration , function() {});
+  }, function() {
+    var img = $(this);
+    img.children("p.wrap").animate({
+      height: options.height + 'px'
+    }, options.toggleDuration , function() {});
+    img.children("p.bottom").html("Click to expand image to full height.").animate({
+      top: options.height + 'px'
+    }, options.toggleDuration );
+  });
+};
---
+++ readme.md
@@ -0,0 +1,11 @@
+#jQuery Image Slider
+
+The jQuery Image Slider is a piece of jQuery code I produced during the second year of my [IMD Course](http://imdweb.infj.ulst.ac.uk/). It came about because whilst I wanted to make use of large images, it was typically frowned upon as being used for 'filler' on the site. To that end I came up with a method to load in large images whilst not showing the full piece to the user until they decided to view it.
+
+Whilst it started out as a very rough and ready (and admittedly sloppy) piece of code, I have cleaned it up somewhat and made sure that it works in as flexible a manner as possible. This is something I intend to continue in the future.
+
+v0.4 - Cleaned up performance of code.
+v0.3 - Improved functionality of plugin, made it easier to use, and started making use of the HTML5 ability to use data- to create custom attributes to HTML elements.
+v0.2 - Added support for other elements than div (mainly for support of the html5 figure element) [released on web & github]
+v0.1 - Converted from raw jQuery to a jQuery Plugin [released on web]
+v0.0 - Basic Functionality for personal use
